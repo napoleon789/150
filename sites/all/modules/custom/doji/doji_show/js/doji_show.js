09db8a557ba2aed05b4faeb874e57e2b1ed0c81f
Hà Minh <hoangminh.ivn@outlook.com>
2016-08-18 10:27:22 +0700

    add

--- sites/all/modules/custom/doji/doji_show/js/doji_show.js
+++ sites/all/modules/custom/doji/doji_show/js/doji_show.js
@@ -1,3 +1,28 @@
 jQuery(document).ready(function() {
-
+    $("#block-block-6 .form-select").change(function() {
+        var uid = $(this).val();
+        console.log(uid);
+        var url = 'sites/default/files/data/hienthi/ant-32572_vungmien_'+uid+'.dat';
+        $.ajax({
+            url: url,
+            type: "GET",
+            success: function(data) {
+                console.log(data);
+                $('.show_ex').html(data);
+            }
+        })
+    });
+    //thay doi hop select trang chu cho user khong dang nhap
+    $("#block-block-3 .form-select").change(function() {
+        var uid = $(this).val();
+        console.log(uid);
+        var url = 'sites/default/files/data/hienthi/ant-32572_vungmien_'+uid+'.dat';
+        $.ajax({
+            url: url,
+            type: "GET",
+            success: function(data) {
+                $('.dn.midle').html(data);
+            }
+        })
+    });
 });
